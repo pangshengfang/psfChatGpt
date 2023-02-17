b2eb7d8a2bf2366cb79ba8f8aa8d271b770dc473
Yuan Zhong <yuanzhong@YuandeMacBook-Pro.local>
2023-02-18 02:05:30 +0800

    init 2

--- src/test/java/com/example/psfchatgpt/PsfChatGptApplicationTests.java
+++ src/test/java/com/example/psfchatgpt/PsfChatGptApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.psfchatgpt;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PsfChatGptApplicationTests {
+public class PsfChatGptApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
